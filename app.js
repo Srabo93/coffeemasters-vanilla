fefe9f5e6602b212e0600e150f69cc80588635da
Srabo93 <a.s-dev@web.de>
2023-11-28 22:24:45 +0100

    finished router and form 2way binding

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import Router from "./services/Router.js";
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailPage } from "./components/DetailPage.js";
 import { OrderPage } from "./components/OrderPage.js";
+import ProductItem from "./components/ProductItem.js";
+import CartItem from "./components/CartItem.js";
 
 
 window.app={}
@@ -13,4 +15,14 @@ app.router = Router
 window.addEventListener("DOMContentLoaded", () => {
   loadData()
  app.router.init() 
+})
+
+window.addEventListener("appcartchange",()=> {
+ const badge = document.getElementById('badge')
+ const qty = app.store.cart.reduce((acc,item)=> 
+  acc + item.quantity, 0
+ )
+
+ badge.textContent = qty
+ badge.hidden = qty == 0;
 })
